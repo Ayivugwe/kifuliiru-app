b932f6ebc1407f2e9a6606971dd6b0be256e9e4a
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2022-02-14 08:59:02 +0200

    Change in kifuliiru.dart

--- lib/kifuliiru.dart
+++ lib/kifuliiru.dart
@@ -14,9 +14,9 @@ class Kifuliiru extends StatelessWidget {
           ),
         ),
         actions: [
-          Text('1'),
-          Text('2'),
-          Text('3'),
+          Text('Ibufuliiru'),
+          Text('Kifuliiru'),
+          Text('Bafuliiru'),
         ],
         backgroundColor: Colors.white,
       ),
@@ -41,7 +41,10 @@ class Kifuliiru extends StatelessWidget {
               ),
             ),
             Text(
-                'Tumenye imyazi yoshi ye\' Kifuliiru. Abafuliiru tuli bandu tutuziri Ibufuliiru tumudeta indeto imuhamagalwa Kifuliiru. Indeto yito yotumuhikangatira imuhamagalwa Kifuliiru. Tumenye bingi higulu lya Bafuliiru na byoshi ibibaloziri.'),
+                'Tumenye imyazi yoshi ye\' Kifuliiru. Abafuliiru tuli bandu tutuziri 
+                Ibufuliiru tumudeta indeto imuhamagalwa Kifuliiru. Indeto yito 
+                yotumuhikangatira imuhamagalwa Kifuliiru. Tumenye bingi higulu 
+                lya Bafuliiru na byoshi ibibaloziri.'),
             Text(
               'Abamuganula i Kifuliiru',
               style: TextStyle(
@@ -51,7 +54,14 @@ class Kifuliiru extends StatelessWidget {
               ),
             ),
             Text(
-                'I Kifuliiru iliiri  ndeto nga zindi ndeto zoshi. Kili kinovu kubabutirwa bayo na kubalya abatuziiri Ibufuliiru kili bangaba batali Bafuliiru  na kubalya abatuziiri kuguma na Bafuliiru kandi li hofi lye Bufuliiru. Mubalya abamuganula i Kifuliiru yo na ndeto iliiri hofi bwnene ne Kifuliiru bali bene witu bamuhamagalya Banyindu. Bamuganula indeto imuhamgalwa Kinyindu. Bano bali hofi litu bwnene ne  ndeto yabo iliri nga yo yimwitu naho Kifuliiru, haliko yizi ndeto zibiri zisigini hanini.'),
+                'I Kifuliiru iliiri  ndeto nga zindi ndeto zoshi. 
+                Kili kinovu kubabutirwa bayo na kubalya abatuziiri Ibufuliiru 
+                kili bangaba batali Bafuliiru  na kubalya abatuziiri kuguma na 
+                Bafuliiru kandi li hofi lye Bufuliiru. Mubalya abamuganula i Kifuliiru 
+                yo na ndeto iliiri hofi bwnene ne Kifuliiru bali bene 
+                witu bamuhamagalya Banyindu. Bamuganula indeto imuhamgalwa Kinyindu. 
+                Bano bali hofi litu bwnene ne  ndeto yabo iliri nga yo yimwitu 
+                naho Kifuliiru, haliko yizi ndeto zibiri zisigini hanini.'),
             Text(
               'Ndeto izishushini ne Kifuliiru',
               style: TextStyle(
@@ -61,7 +71,13 @@ class Kifuliiru extends StatelessWidget {
               ),
             ),
             Text(
-                'I Kifuliiru iliiri  ndeto nga zindi ndeto zoshi. Kili kinovu kubabutirwa bayo na kubalya abatuziiri Ibufuliiru kili bangaba batali Bafuliiru  na kubalya abatuziiri kuguma na Bafuliiru kandi li hofi lye Bufuliiru. Mubalya abamuganula i Kifuliiru yo na ndeto iliiri hofi bwnene ne Kifuliiru bali bene witu bamuhamagalya Banyindu. Bamuganula indeto imuhamgalwa Kinyindu. Bano bali hofi litu bwnene ne  ndeto yabo iliri nga yo yimwitu naho Kifuliiru, haliko yizi ndeto zibiri zisigini hanini.'),
+                'I Kifuliiru iliiri  ndeto nga zindi ndeto zoshi. Kili kinovu kubabutirwa 
+                bayo na kubalya abatuziiri Ibufuliiru kili bangaba batali Bafuliiru 
+                na kubalya abatuziiri kuguma na Bafuliiru kandi li hofi lye Bufuliiru. 
+                Mubalya abamuganula i Kifuliiru yo na ndeto iliiri hofi bwnene ne 
+                Kifuliiru bali bene witu bamuhamagalya Banyindu. Bamuganula indeto 
+                imuhamgalwa Kinyindu. Bano bali hofi litu bwnene ne  ndeto yabo iliri 
+                nga yo yimwitu naho Kifuliiru, haliko yizi ndeto zibiri zisigini hanini.'),
             Text(
               'Tukole mu Kifuliiru',
               style: TextStyle(
@@ -71,7 +87,13 @@ class Kifuliiru extends StatelessWidget {
               ),
             ),
             Text(
-                'Ukukola kuliri kuhamu bwenene. Ukudeta kwo ahandu hagendere kandi li abandu bagenderere, babanini ukukola. Nitu higulu lye\'mwitu Ibufuliiru, higulu lya\'abandu biitu Abafuliiru na higulu lye\'ndeto yiitu Kifuliiru; tubanini ukukola bwenene no kubika imisi yiitu yooshi. Ibija byooshi twangahikira bitagashaga mu kubwatala naho kandi li mu miganulo ya kwokwokwo, haliko tubanini tukole.'),
+                'Ukukola kuliri kuhamu bwenene. Ukudeta kwo ahandu hagendere
+                 kandi li abandu bagenderere, babanini ukukola. Nitu higulu lye\'mwitu 
+                 Ibufuliiru, higulu lya\'abandu biitu Abafuliiru na higulu 
+                 lye\'ndeto yiitu Kifuliiru; tubanini ukukola bwenene no 
+                 kubika imisi yiitu yooshi. Ibija byooshi twangahikira 
+                 bitagashaga mu kubwatala naho kandi li mu miganulo ya kwokwokwo, 
+                 haliko tubanini tukole.'),
           ],
         ),
       ),
