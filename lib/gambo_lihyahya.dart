071fe975684d7627d2868e32af6dacd95b32ddf3
Ayivugwe Kabemba <ayivugwekabemba@gmail.com>
2024-05-08 04:01:45 -0700

    add Supabase to app

--- lib/gambo_lihyahya.dart
+++ lib/gambo_lihyahya.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'igambo.dart';
 
 class GamboLihyahya extends StatelessWidget {
   const GamboLihyahya({Key? key}) : super(key: key);
 
-  get database => null;
-
   @override
   // ignore: avoid_renaming_method_parameters
   Widget build(BuildContext ctxt) {
-    final txtControllerKifuliiru = TextEditingController(),
-        txtControllerKifuliiruKifuliiru = TextEditingController(),
-        txtControllerKifuliiruKiswahili = TextEditingController(),
-        txtControllerKifuliiruKifaransa = TextEditingController(),
-        txtControllerKifuliiruKingereza = TextEditingController();
+    final txtControllerIgambo = TextEditingController(),
+        txtControllerKifuliiru = TextEditingController(),
+        txtControllerKiswahili = TextEditingController(),
+        txtControllerKifaransa = TextEditingController(),
+        txtControllerKingereza = TextEditingController();
 
-    Igambo igambo;
-    List<Igambo> listeMagmbo = [];
+    Igambo gaambo;
+    List<Igambo> listeMagambo = [];
     String gamboMuKifuliiru,
         gamboKifuliiruKifuliiru,
         gamboKifuliiruKiswahili,
@@ -38,8 +37,7 @@ class GamboLihyahya extends StatelessWidget {
         ),
         body: SafeArea(
             child: SingleChildScrollView(
-              padding: const EdgeInsets.symmetric(horizontal: 32),
-            
+          padding: const EdgeInsets.symmetric(horizontal: 32),
           child: Padding(
             padding: const EdgeInsets.all(10),
             child: Center(
@@ -49,7 +47,7 @@ class GamboLihyahya extends StatelessWidget {
                     height: 50,
                   ),
                   const Text(
-                      'Yandika ngisi gambo lyo\'yiji mu ndeto ngisi kwo zikulirikirini'),
+                      'Yandika ngisi gambo lyo\'yiji mu ndeto ngisi kwo zikulikirini'),
                   const SizedBox(
                     height: 15,
                   ),
@@ -57,9 +55,9 @@ class GamboLihyahya extends StatelessWidget {
                     keyboardType: TextInputType.text,
                     minLines: 1,
                     maxLines: 5,
-                    controller: txtControllerKifuliiru,
+                    controller: txtControllerIgambo,
                     decoration: InputDecoration(
-                      labelText: 'Igambo mu Kifuliiru*',
+                      labelText: 'Igambo mu Kifuliiru *',
                       fillColor: Colors.white,
                       focusedBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.circular(10.0),
@@ -103,7 +101,7 @@ class GamboLihyahya extends StatelessWidget {
                         ),
                       ),
                     ),
-                    controller: txtControllerKifuliiruKifuliiru,
+                    controller: txtControllerKifuliiru,
                   ),
                   const SizedBox(
                     height: 15,
@@ -133,7 +131,7 @@ class GamboLihyahya extends StatelessWidget {
                     ),
                     showCursor: true,
 
-                    controller: txtControllerKifuliiruKiswahili,
+                    controller: txtControllerKiswahili,
                   ),
                   const SizedBox(
                     height: 15,
@@ -162,7 +160,7 @@ class GamboLihyahya extends StatelessWidget {
                           ),
                         ),
                       ),
-                      controller: txtControllerKifuliiruKifaransa),
+                      controller: txtControllerKifaransa),
                   const SizedBox(
                     height: 15,
                   ),
@@ -190,42 +188,31 @@ class GamboLihyahya extends StatelessWidget {
                           ),
                         ),
                       ),
-                      controller: txtControllerKifuliiruKingereza),
+                      controller: txtControllerKingereza),
                   const SizedBox(
                     height: 20,
                   ),
                   ElevatedButton(
                       onPressed: () async {
                         gamboMuKifuliiru = txtControllerKifuliiru.text;
-                        gamboKifuliiruKifuliiru =
-                            txtControllerKifuliiruKifuliiru.text;
-                        gamboKifuliiruKiswahili =
-                            txtControllerKifuliiruKiswahili.text;
-                        gamboKifuliiruKifaransa =
-                            txtControllerKifuliiruKifaransa.text;
-                        gamboKifuliiruKingereza =
-                            txtControllerKifuliiruKingereza.text;
+                        gamboKifuliiruKifuliiru = txtControllerKifuliiru.text;
+                        gamboKifuliiruKiswahili = txtControllerKiswahili.text;
+                        gamboKifuliiruKifaransa = txtControllerKifaransa.text;
+                        gamboKifuliiruKingereza = txtControllerKingereza.text;
 
-                        igambo = Igambo(
+                        gaambo = Igambo(
                             igambo: gamboMuKifuliiru,
                             kifuliiru: gamboKifuliiruKifuliiru,
                             kiswahili: gamboKifuliiruKiswahili,
                             kifaransa: gamboKifuliiruKifaransa,
                             kingereza: gamboKifuliiruKingereza);
 
-                        insertData(
-                            gamboMuKifuliiru,
-                            gamboKifuliiruKifuliiru,
-                            gamboKifuliiruKiswahili,
-                            gamboKifuliiruKifaransa,
-                            gamboKifuliiruKingereza);
-
-                        listeMagmbo.add(igambo);
-
-                        // Close the database
-                        await database.close();
+                        await Supabase.instance.client
+                            .from('magambo')
+                            .insert(gaambo);
+                        //listeMagambo.add(gaambo);
                       },
-                      child: const Text('Bika'))
+                      child: const Text('Bika bino wayandika'))
                 ],
               ),
             ),
@@ -233,16 +220,9 @@ class GamboLihyahya extends StatelessWidget {
         )));
   }
 
-  insertData(
-      String kifuliiru,
-      String kifuliiruKifuliiru,
-      String kifuliiruKiswahili,
-      String kifuliiruKifaransa,
-      String kifuliiruKingereza) async {}
-
+/* 
   updateData(int id, String kifuliiru, String kifKif, String kifKiswah,
       String kifKifar, String kifKinge) async {}
-
   getRecords() async {
 // Get the records
     List<Map> list =
@@ -260,5 +240,5 @@ class GamboLihyahya extends StatelessWidget {
 
   deleteData() async {
 // Delete a record
-  }
+  } */
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,17 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 import 'intro_screen.dart';
-
-//import 'package:carousel_slider/carousel_slider.dart';
-void main() => runApp(MyApp());
+Future<void> main() async {
+  await Supabase.initialize(url: 'https://jzylueotpenjojusudox.supabase.co', anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6eWx1ZW90cGVuam9qdXN1ZG94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTgwOTk3MTEsImV4cCI6MjAxMzY3NTcxMX0.KfapliWx6pFxGYmil2B2Gy0-aJtzV7kOMrhgU2lyvSA');
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: IntroScreen(), //load intro screen
     );
